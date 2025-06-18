de5ce46fa2df47dc7adc103d31a89482638330f2
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-06-18 08:43:32 +0500

    added12

---
+++ index.js
@@ -0,0 +1,42 @@
+export let data = [
+    {
+        color: '#014E56',
+        text: "Midnight Green",
+        p: "HEX : #014E56 RGB : rgb(1, 78, 86) HSL : hsl(186°, 98%, 17%)"
+    },
+    {
+        color: '#F67E7E',
+        text: "Light Coral",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#004047',
+        text: "White",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#79C8C7',
+        text: "Rapture Blue",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#2C6269',
+        text: "Police Blue",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#012F34',
+        text: "Deep Jungle Green",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#022F28',
+        text: "Sacramento State Green",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    },
+    {
+        color: '#002529',
+        text: "Dark Green",
+        p: 'HEX : #F67E7E RGB : rgb(246, 126, 126) HSL : hsl(0°, 87%, 73%)'
+    }
+]
